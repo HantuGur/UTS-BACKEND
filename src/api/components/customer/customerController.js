4a5baba6867fbdf9748941bbcb173ba4db32b4f8
Chandra Wijaya <chandrapinter25@gmail.com>
2026-04-13 16:32:51 +0700

    Implement customer creation and retrieval endpoints

---
+++ src/api/components/customer/customerController.js
@@ -0,0 +1,22 @@
+const Customer = require("../../../models/customerModel");
+
+// CREATE
+exports.createCustomer = async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.status(201).json(customer);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+// GET ALL
+exports.getCustomers = async (req, res) => {
+  try {
+    const data = await Customer.find();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
