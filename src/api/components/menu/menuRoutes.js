3274a8a93a664d65e4428c99148bede203a03cbc
davvinfusta@gmail.com <davvinfusta@gmail.com>
2026-04-13 16:15:47 +0700

    tambahin menuRoutes dan menuController

---
+++ src/api/components/menu/menuRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const menuController = require('./menuController');
+
+router.post('/', menuController.createMenu);
+router.get('/', menuController.getMenus);
+
+module.exports = router;
