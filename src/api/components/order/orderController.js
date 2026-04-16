186c32d35841e5262d08ebf174664777a037f30c
Aaaron valerio lieputra <Aaronvaleriolee@gmail.com>
2026-04-16 14:01:31 +0700

    benerin error di aaron

--- src/api/components/order/orderController.js
+++ src/api/components/order/orderController.js
@@ -30,3 +30,47 @@ exports.getOrders = async (req, res) => {
     res.status(500).json(err.message);
   }
 };
+
+// GET BY ID
+exports.getOrderById = async (req, res) => {
+  try {
+    const data = await Order.findById(req.params.id);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+// DELETE
+exports.deleteOrder = async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Order deleted' });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+// UPDATE
+exports.updateOrder = async (req, res) => {
+  try {
+    const data = await Order.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(data);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+// FILTER STATUS
+exports.getByStatus = async (req, res) => {
+  try {
+    const data = await Order.find({
+      status: req.params.status,
+    });
+    res.json(data);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
